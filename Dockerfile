26821907e187922a69e5b67abea7e7378708c267
Hellen Avila Rosa <hellenar12@gmail.com>
2021-08-24 14:53:11 -0300

    teste2

--- Dockerfile
+++ Dockerfile
@@ -3,6 +3,6 @@ LABEL maintainer="lorenz.vanthillo@gmail.com"
 COPY . /app
 WORKDIR /app
 RUN pip install -r requirements.txt
-EXPOSE 8080
+EXPOSE 80
 ENTRYPOINT ["python"]
 CMD ["app/app.py"]
--- app/app.py
+++ app/app.py
@@ -6,12 +6,14 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     try:
-        host_name = socket.gethostname()
-        host_ip = socket.gethostbyname(host_name)
+        # host_name = socket.gethostname()
+        # host_ip = socket.gethostbyname(host_name)
+        host_name = 'hellen'
+        host_ip = '123'
         return render_template('index.html', hostname=host_name, ip=host_ip)
     except:
         return render_template('error.html')
 
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=8080)
+    app.run(host='0.0.0.0', port=80)
