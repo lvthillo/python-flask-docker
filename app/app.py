ed2f211994d7b7c87a4d2dd9908f34d482b2f708
Hellen Avila Rosa <hellenar12@gmail.com>
2021-09-01 14:38:24 -0300

    teste2

--- app/app.py
+++ app/app.py
@@ -1,15 +1,18 @@
 from flask import Flask,render_template
 import socket
-
+import time
 app = Flask(__name__)
 
 @app.route("/")
 def index():
+    
     try:
         # host_name = socket.gethostname()
         # host_ip = socket.gethostbyname(host_name)
         host_name = 'hellen'
         host_ip = '123'
+        host_ip = 'test'
+        time.sleep(50)
         return render_template('index.html', hostname=host_name, ip=host_ip)
     except:
         return render_template('error.html')
