eb90a7a8ba3c55f061d6d75b697dcea715c67825
Hellen Avila Rosa <hellenar12@gmail.com>
2021-09-02 16:56:36 -0300

    test

--- app/app.py
+++ app/app.py
@@ -14,25 +14,25 @@ def index():
         host_name = 'hellen'
         host_ip = '123'
         host_ip = 'test'
-        time.sleep(50)
         return render_template('index.html', hostname=host_name, ip=host_ip)
     except:
         return render_template('error.html')
 
-def load_kinesis_record():
-    kinesis_record = None
-    try:
-        if kinesis_record == 1:
-            kinesis_record = subprocess.call(cmd, shell=True)
-        else:
-            kinesis_record = eval(cmd)
-        successes += 1
-    except Exception as e:
-        pass
-    return kinesis_record
+# def load_kinesis_record():
+#     kinesis_record = None
+#     try:
+#         if kinesis_record == 1:
+#             kinesis_record = subprocess.call(cmd, shell=True)
+#         else:
+#             kinesis_record = eval(cmd)
+#         successes += 1
+#     except Exception as e:
+#         pass
+#     return kinesis_record
 
 
 if __name__ == "__main__":
-    Profiler(profiling_group_name='MyProfilingGroup').start()
+    custom_session = boto3.session.Session(profile_name='dev', region_name='us-west-2')
+    Profiler(profiling_group_name="PythonProfiler", aws_session=custom_session).start()
     start_application()
     app.run(host='0.0.0.0', port=8080)
