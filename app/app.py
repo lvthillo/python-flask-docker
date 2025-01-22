077ebc14c51db21d26e9e508139db492e7c1753a
Regev Golan <regevg@regevg-mbp-1.local>
2025-01-22 10:44:13 +0200

    [CI] Add pylint and black

--- app/app.py
+++ app/app.py
@@ -1,5 +1,6 @@
-from flask import Flask, render_template
 import socket
+from flask import Flask, render_template
+
 
 app = Flask(__name__)
 
@@ -9,9 +10,13 @@ def index():
     try:
         host_name = socket.gethostname()
         host_ip = socket.gethostbyname(host_name)
-        return render_template('index.html', hostname=host_name, ip=host_ip)
-    except Exception:
-        return render_template('error.html')
+        return render_template("index.html", hostname=host_name, ip=host_ip)
+    except socket.gaierror:
+        return render_template(
+            "error.html", message="Unable to resolve hostname to IP address."
+        )
+    except Exception:  # pylint: disable=broad-except
+        return render_template("error.html")
 
 
 @app.route("/health")
@@ -25,6 +30,5 @@ def greet_user(user_id):
 
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=8080)
+    app.run(host="0.0.0.0", port=8080)
     print("Service is shutting down..")
-
