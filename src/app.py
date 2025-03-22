14246dba7c00d9c13a5a40d87d8dc79062f336fe
Lorenz Vanthillo <lorenz.vanthillo@gmail.com>
2025-03-22 20:48:51 +0100

    style(pfd): run black

--- src/app.py
+++ src/app.py
@@ -1,17 +1,18 @@
-from flask import Flask,render_template
+from flask import Flask, render_template
 import socket
 
 app = Flask(__name__)
+
 
 @app.route("/")
 def index():
     try:
         host_name = socket.gethostname()
         host_ip = socket.gethostbyname(host_name)
-        return render_template('index.html', hostname=host_name, ip=host_ip)
+        return render_template("index.html", hostname=host_name, ip=host_ip)
     except:
-        return render_template('error.html')
+        return render_template("error.html")
 
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=8080)
+    app.run(host="0.0.0.0", port=8080)
